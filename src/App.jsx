e5f107575eb33c61e442a19ef19f00308e52b74d
Malene Remm <malene.remm@hotmail.com>
2026-01-10 22:10:18 +0100

    fix: correct punctuation in info message

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <header>
           <h1 className="title">AvaKlinikken</h1>
         </header>
-        <p className="info">Kommer snart</p>
+        <p className="info">Kommer snart!</p>
        <a className="contact-link" href="mailto:post@avaklinikken.no">Kontakt oss</a>
       </main>
     </>
