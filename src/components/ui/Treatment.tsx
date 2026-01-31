55a8b585316db49acc49a8506177bc97404cf6ce
Malene Remm <malene.remm@hotmail.com>
2026-01-31 15:14:30 +0100

    feat: reorder elements in Treatment component for improved layout

--- src/components/ui/Treatment.tsx
+++ src/components/ui/Treatment.tsx
@@ -10,10 +10,10 @@ interface TreatmentProps {
 export const Treatment = ({imageUrl, title, description} : TreatmentProps) => {
   return (
     <article className="flex flex-col gap-4 m-4">
-        <h3 className="text-2xl">{title}</h3>
       <div>
         <img className="rounded-md" src={imageUrl} alt={title} />
       </div>
+        <h3 className="text-2xl">{title}</h3>
         <p>{description}</p>
         <Button className="border bg-brown-500 text-white p-2 rounded-md" onClick={() => {}}>BOOK BEHANDLING</Button>
     </article>
