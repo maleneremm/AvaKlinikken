13700e7c56c41011916b11b4f6d2c047186dd4a0
Malene Remm <malene.remm@hotmail.com>
2026-01-31 21:41:33 +0100

    feat: update Contact Us section with email and phone links for better accessibility

--- src/components/ui/ContactUs.tsx
+++ src/components/ui/ContactUs.tsx
@@ -6,10 +6,16 @@ export const ContactUs = () => {
     <footer className="bg-white p-4">
       <h3 className="text-2xl">Kontakt oss</h3>
       <ul className="flex flex-col gap-2 mt-2 items-center">
-        <li>E-post: post@avaklinikken.no</li>
-        <li>Telefon: +47 455 74 879</li>
+        <li >
+          E-post: <a href="mailto:post@avaklinikken.no">post@avaklinikken.no</a>
+        </li>
+        <li>Telefon: <a href="tel:">+47 123 45 678</a></li>
         <li>Adresse: Sannergata 32B, 0557 Oslo</li>
-        <li className="flex gap-2" onClick={visitInstagram} style={{ cursor: "pointer" }}   >
+        <li
+          className="flex gap-2"
+          onClick={visitInstagram}
+          style={{ cursor: "pointer" }}
+        >
           <Instagram />
           Følg oss på Instagram!
         </li>
