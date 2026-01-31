0027e952ca422a769bf2825c6b9df6dc9a28acc9
Malene Remm <malene.remm@hotmail.com>
2026-01-31 16:38:34 +0100

    feat: update team section with new employee images and descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import wrinkleTreatment from "../src/assets/wrinkle-treatment.png";
 import fillers from "../src/assets/fillers.png";
 import { About } from "./components/ui/About";
 import { ContactUs } from "./components/ui/ContactUs";
+import employeeAnna from "../src/assets/employee-Anna.png";
+import employeeHannan from "../src/assets/employee-Hannan.png";
 
 function App() {
   return (
@@ -14,7 +16,7 @@ function App() {
       <header>
         <Navbar />
       </header>
-      <main>  
+      <main>
         <Section>
           <Hero />
         </Section>
@@ -23,19 +25,41 @@ function App() {
           <h1 className="text-3xl w-screen bg-white p-4">Behandlinger</h1>
           <Treatment
             title="Rynkebehandling"
-            description={["Kråketær/smilerynker", "Glabella/sinnarynker", "Pannerynker", "Masseter/kjeve", "Marionette/munnvik", "Brynsløft"]}  
+            description={[
+              "Kråketær/smilerynker",
+              "Glabella/sinnarynker",
+              "Pannerynker",
+              "Masseter/kjeve",
+              "Marionette/munnvik",
+              "Brynsløft",
+            ]}
             imageUrl={wrinkleTreatment}
           />
           <Treatment
             title="Fillers (opptil 1 ml)"
-            description={["Kjeve", "Hake", "Kinnbein", "Nasolabiale linjer", "Marionette linjer", "Lepper"]}
+            description={[
+              "Kjeve",
+              "Hake",
+              "Kinnbein",
+              "Nasolabiale linjer",
+              "Marionette linjer",
+              "Lepper",
+            ]}
             imageUrl={fillers}
           />
         </Section>
         <Section>
           <h1 className="text-3xl w-screen bg-white p-4">Vårt team</h1>
-         <About imageUrl="../src/assets/employee-Anna.png" name="Anna" description="Anna ble utdannet sykepleier i 2024. Hun har det siste året fått sertifiseringer for botulinumtoksin og forskjellige filler. Anna har stor interesse for faget og har fokus på kundens ønske, samt naturlige restulateter." />
-         <About imageUrl="../src/assets/employee-Hannan.JPG" name="Hannan" description="Hannan er en erfaren sykepleier med over XX år erfaring. Hun har det siste året utført flere sertifiseringer innnen estetisk medisin, i behandlinger som fillers og botulinumtoksin. Hannan fokuserer på din unike hudtype, ser hele mennesket og er presis og tydelig i sine behandlinger." />
+          <About
+            imageUrl={employeeAnna}
+            name="Anna"
+            description="Anna ble utdannet sykepleier i 2024. Hun har det siste året fått sertifiseringer for botulinumtoksin og forskjellige filler. Anna har stor interesse for faget og har fokus på kundens ønske, samt naturlige restulateter."
+          />
+          <About
+            imageUrl={employeeHannan}
+            name="Hannan"
+            description="Hannan er en erfaren sykepleier med over XX år erfaring. Hun har det siste året utført flere sertifiseringer innnen estetisk medisin, i behandlinger som fillers og botulinumtoksin. Hannan fokuserer på din unike hudtype, ser hele mennesket og er presis og tydelig i sine behandlinger."
+          />
         </Section>
         <Section>
           <ContactUs />
