192f592c8161d2fb1ca47216c43b2a0adea93c7b
Malene Remm <malene.remm@hotmail.com>
2026-01-31 13:29:40 +0100

    feat: add Treatment component with props for image, title, and description

---
+++ src/components/ui/Treatment.tsx
@@ -0,0 +1,12 @@
+interface TreatmentProps {
+  imageUrl?: string;
+  title?: string;
+  description?: string;
+  
+}
+
+export const Treatment = () => {
+  return (
+    <div>Treatments</div>
+  )
+}
