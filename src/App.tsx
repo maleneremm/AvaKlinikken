bbed3edf79d5bf4cbc9b3e16a634fcdd082fc343
Malene Remm <malene.remm@hotmail.com>
2026-01-31 14:01:09 +0100

    feat: add Treatment section with images, titles, and descriptions for wrinkle and filler treatments

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Navbar } from "./components/common/Navbar";
 import Section from "./components/layout/Section";
 import {Hero} from "./components/ui/Hero";
 import { Treatment } from "./components/ui/Treatment";
+import wrinkleTreatment from "../src/assets/wrinkle-treatment.png";
+import fillers from "../src/assets/fillers.png";
 
 function App() {
   return (
@@ -14,8 +16,11 @@ function App() {
         <Section>
           <Hero />
         </Section>
+    
         <Section>
-          <Treatment />
+          <h1 className="text-3xl w-screen bg-white p-4">Behandlinger</h1>
+          <Treatment title="Rynkebehandling" description="Kråketær/smilerynker, Glabella/sinnarynker, Pannerynker, Masseter/kjeve, Marionette/munnvik, Brynsløft" imageUrl={wrinkleTreatment} />
+           <Treatment title="Fillers" description="Kjeve, Hake, Kinnbein, Nasolabiale linjer, Marionette linjer, Lepper" imageUrl={fillers} />
         </Section>
       </main>
     </>
--- src/components/ui/Treatment.tsx
+++ src/components/ui/Treatment.tsx
@@ -1,12 +1,21 @@
+import { Button } from "../common/Button";
+
 interface TreatmentProps {
   imageUrl?: string;
   title?: string;
   description?: string;
-  
+
 }
 
-export const Treatment = () => {
+export const Treatment = ({imageUrl, title, description} : TreatmentProps) => {
   return (
-    <div>Treatments</div>
+    <article className="flex flex-col gap-4 m-4">
+        <h3 className="text-2xl">{title}</h3>
+      <div>
+        <img className="rounded-md" src={imageUrl} alt={title} />
+      </div>
+        <p>{description}</p>
+        <Button className="border bg-brown-500 text-white p-2 rounded-md" onClick={() => {}}>BOOK BEHANDLING</Button>
+    </article>
   )
 }
