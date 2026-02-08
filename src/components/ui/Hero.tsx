f4bbb73ebe844f6122ec4346aee126682b2c2d71
Malene Remm <malene.remm@hotmail.com>
2026-02-08 23:00:49 +0100

    fix: adjust Hero component image width and padding; improve text formatting for clarity

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -3,15 +3,20 @@ import heroBanner from "../../assets/hero-section.jpg";
 export const Hero = () => {
   return (
     <div>
-      <img src={heroBanner} alt="Hero Banner" className="w-screen" />
-      <div className="flex flex-col gap-4 items-center p-4 lg:p-22 lg:gap-8">
+      <img
+        src={heroBanner}
+        alt="Hero Banner"
+        className="w-full md:w-3/4 place-self-center"
+      />
+      <div className="flex flex-col gap-4 items-center p-4 lg:p-8 lg:gap-2">
         <header>
           <h2 className="text-2xl lg:text-4xl">Velkommen til</h2>
           <h2 className="text-2xl font-bold lg:text-4xl">AvaKlinikken</h2>
         </header>
         <p className="text-center md:text-2xl lg:max-w-xl">
-         Skjønnhetsklinikk som driver med estetiske og medisinske behandlinger. Våre åpningstider er fleksible. Se ledige timer i Timma/under «Book
-          time». {" "}
+          Skjønnhetsklinikk som driver med estetiske og medisinske behandlinger.
+          Våre åpningstider er fleksible. Se ledige timer i Timma/under «Book
+          time».{" "}
           <a
             href="mailto:post@avaklinikken.no"
             className="font-semibold underline underline-offset-2 decoration-[0.5px] decoration-stone-600 md:decoration-transparent md:hover:decoration-current transition"
